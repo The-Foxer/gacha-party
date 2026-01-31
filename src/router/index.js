5a653ecb5ba1b67c11b664bfb92cfa96366fb6c1
The-Foxer <The_foxer01@Foxmail.com>
2026-01-31 23:10:20 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CharacterList from '@/views/CharacterList.vue'
 import CharacterDetail from '@/views/CharacterDetail.vue'
-import DataConverter from '@/views/DataConverter.vue'  // 新增
-import aidata from '@/views/AiDataConverter.vue'
-import skillai from '@/views/SkillAiConverter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
